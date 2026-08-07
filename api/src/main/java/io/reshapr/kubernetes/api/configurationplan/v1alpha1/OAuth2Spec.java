c03f874b3196f211edce582ba6bbf9ebee2db9db
Vaishnav Kale <116202759+Vaishnav88sk@users.noreply.github.com>
2026-08-07 19:14:58 +0530

    feat: implement ConfigurationPlan CRD and Reconciler (#16)
    Signed-off-by: Vaishnav88sk <vaishnavsk8804@gmail.com>

---
+++ api/src/main/java/io/reshapr/kubernetes/api/configurationplan/v1alpha1/OAuth2Spec.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright The Reshapr Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.reshapr.kubernetes.api.configurationplan.v1alpha1;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import io.sundr.builder.annotations.Buildable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Buildable(editableEnabled = false, builderPackage = "io.fabric8.kubernetes.api.builder")
+public class OAuth2Spec {
+
+    @JsonPropertyDescription("OAuth2 Client ID")
+    private String clientId;
+
+    @JsonPropertyDescription("OAuth2 Client Secret")
+    private String clientSecret;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+}
